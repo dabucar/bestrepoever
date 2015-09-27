56ad42e9bc43c877c78ea7ca2c2af145169d8229
Silvan Jegen <s.jegen@gmail.com>
2015-09-27 20:53:28 +0200

    Allow more than one input file
    We iterate over all input files given on the command line printing the
    desired information. Diagnostic info like the set of XML tags will be
    printed to stderr.
    
    We also simplify the None check while we are at it.

--- parse_XML_file.py
+++ parse_XML_file.py
@@ -8,47 +8,47 @@ import os.path
 import sys
 import xml.etree.ElementTree as ET
 
-# check if argument provided
+# check if arguments have been provided
 if not len(sys.argv) > 1:
-	print "Provide a file, bitte"
+	print("Provide at least one file, bitte", file=sys.stderr)
 	sys.exit()
 
-# check if argument is a valid file
-file_path = sys.argv[1]
-if not os.path.isfile(file_path):
-	print "Not a valid file, gtfo"
-	sys.exit()
+for file_path in sys.argv[1:]:
+    # check if argument is a valid file
+    if not os.path.isfile(file_path):
+            print("Not a valid file, gtfo", file=sys.stderr)
+            sys.exit()
 
-tree = ET.parse(file_path)
+    tree = ET.parse(file_path)
 
-# enlist the collection of tags
-elem_set = set()
-for elem in tree.iter():
-	elem_set.add(elem.tag)
-print sorted(list(elem_set))
+    # enlist the collection of tags
+    elem_set = set()
+    for elem in tree.iter():
+            elem_set.add(elem.tag)
+    print(sorted(list(elem_set)), file=sys.stderr)
 
-# journal title
-res = tree.findall(".//journal-title")
-if not res is None:
-	journal_title = res[0].text 
-# journal issn
-res = tree.findall(".//issn")
-if not res is None:
-	journal_issn = res[0].text 
+    # journal title
+    res = tree.findall(".//journal-title")
+    if res:
+            journal_title = res[0].text
+    # journal issn
+    res = tree.findall(".//issn")
+    if res:
+            journal_issn = res[0].text
 
-# article title
-res = tree.findall(".//article-title")
-if not res is None:
-	article_title = res[0].text 
+    # article title
+    res = tree.findall(".//article-title")
+    if res:
+            article_title = res[0].text
 
-# authors
-res = tree.findall(".//*[@contrib-type='author']/name")
-if not res is None:
-	authors = []
-	for author in res:
-		surname = author.find("surname").text
-		given_names = author.find("given-names").text
-		authors.append((surname, given_names))
+    # authors
+    res = tree.findall(".//*[@contrib-type='author']/name")
+    if res:
+            authors = []
+            for author in res:
+                    surname = author.find("surname").text
+                    given_names = author.find("given-names").text
+                    authors.append((surname, given_names))
 
-print journal_title, journal_issn, article_title, authors
+    print(journal_title, journal_issn, article_title, authors)
 
